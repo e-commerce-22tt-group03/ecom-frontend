64592e8ce628ae7c2940058da661ccd23d3e60ea
Truong Chi Nhan <nhanbin03@gmail.com>
2025-08-01 17:42:08 +0700

    hotfix: Add authorization header jwt

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,10 +1,25 @@
 import axios from 'axios';
+import { store } from '../app/store';
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
   },
+});
+
+api.interceptors.request.use((config) => {
+  // Get the token from the Redux store's state
+  const token = store.getState().auth.token;
+
+  // If a token exists, add it to the Authorization header
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+}, (error) => {
+  return Promise.reject(error);
 });
 
 export default api;
