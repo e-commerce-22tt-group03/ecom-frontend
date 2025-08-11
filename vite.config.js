bda28b3973669ba8762e9a237632d9e9913f1679
Nantas <thinhhochanh2003@gmail.com>
2025-08-12 00:40:39 +0700

    fix the temporary ver of proxy in vite.config.js|| Complete integration

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite' 
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api/v1': {
-        target: 'http://localhost:3001', // adjust if your BE runs elsewhere
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
 })
